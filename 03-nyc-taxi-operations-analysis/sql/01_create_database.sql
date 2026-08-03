ca4ca1a7ed6257ae8520ebe4f43f9ec8ca6d5cc2
PavelBoyko-beep <74496407+PavelBoyko-beep@users.noreply.github.com>
2026-08-03 21:07:59 +0300

    Create 01_create_database.sql

---
+++ 03-nyc-taxi-operations-analysis/sql/01_create_database.sql
@@ -0,0 +1,5 @@
+-- Create a dedicated PostgreSQL database for the project.
+-- Run this command while connected to another database,
+-- such as the default postgres database.
+
+CREATE DATABASE nyc_taxi_operations;
